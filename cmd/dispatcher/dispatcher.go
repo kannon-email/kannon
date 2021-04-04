90faea11c76b4cc68fe3efa8ec4ea80b5394d16e
Ludovico Russo <ludovico@ludusrusso.space>
2021-04-04 17:22:21 +0200

    add statistics with logs

--- cmd/dispatcher/dispatcher.go
+++ cmd/dispatcher/dispatcher.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"time"
 
@@ -8,10 +9,12 @@ import (
 	"github.com/kelseyhightower/envconfig"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/protobuf/proto"
+	pb "kannon.gyozatech.dev/generated/proto"
 	"kannon.gyozatech.dev/internal/db"
 	"kannon.gyozatech.dev/internal/mailbuilder"
 	"kannon.gyozatech.dev/internal/pool"
 
+	"github.com/nats-io/jsm.go"
 	"github.com/nats-io/nats.go"
 )
 
@@ -40,29 +43,81 @@ func main() {
 
 	mb := mailbuilder.NewMailBuilder(dbi)
 
-	nc, err := nats.Connect(config.NatsConn)
+	nc, err := nats.Connect(config.NatsConn, nats.UseOldRequestStyle())
 	if err != nil {
 		logrus.Fatalf("Cannot connect to nats: %v\n", err)
 	}
+	mgr, err := jsm.New(nc)
+	if err != nil {
+		panic(err)
+	}
 
+	go handleErrors(mgr)
+	go handleDelivereds(mgr)
+	dispatcherLoop(pm, mb, nc)
+}
+
+func dispatcherLoop(pm pool.SendingPoolManager, mb mailbuilder.MailBulder, nc *nats.Conn) {
 	for {
 		emails, err := pm.PrepareForSend(100)
 		if err != nil {
 			logrus.Fatalf("cannot prepare for send: %v\n", err)
 		}
-		logrus.Infof("Fetched %v emails\n", len(emails))
+		logrus.Debugf("Fetched %v emails\n", len(emails))
 		for _, email := range emails {
 			data, err := mb.PerpareForSend(email)
 			if err != nil {
 				logrus.Errorf("Cannot send email %v: %v\n", email.To, err)
+				continue
 			}
 			msg, err := proto.Marshal(&data)
 			if err != nil {
 				logrus.Errorf("Cannot send email %v: %v\n", email.To, err)
+				continue
 			}
 			nc.Publish("emails.sending", msg)
 		}
 		logrus.Infof("done sending emails\n")
 		time.Sleep(1 * time.Second)
+	}
+}
+
+func handleErrors(mgr *jsm.Manager) {
+	con, err := mgr.LoadConsumer("kannon", "email-error")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		msg, err := con.NextMsgContext(context.Background())
+		if err != nil {
+			panic(err)
+		}
+		errMsg := pb.Error{}
+		err = proto.Unmarshal(msg.Data, &errMsg)
+		if err != nil {
+			logrus.Errorf("cannot marshal message %v\n", err.Error())
+		}
+		logrus.Printf("[🛑 bump] : %v - %v\n", errMsg.Email, errMsg.Msg)
+		msg.Ack()
+	}
+}
+
+func handleDelivereds(mgr *jsm.Manager) {
+	con, err := mgr.LoadConsumer("kannon", "email-delivered")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		msg, err := con.NextMsgContext(context.Background())
+		if err != nil {
+			panic(err)
+		}
+		deliveredMsg := pb.Delivered{}
+		err = proto.Unmarshal(msg.Data, &deliveredMsg)
+		if err != nil {
+			logrus.Errorf("cannot marshal message %v\n", err.Error())
+		}
+		logrus.Printf("[✅ delivered] : %v\n", deliveredMsg.Email)
+		msg.Ack()
 	}
 }
